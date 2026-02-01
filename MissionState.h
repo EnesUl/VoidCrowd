ae887c3f58974e980adee9df80950abc171f3684
EnesUl <enesuluisik6@gmail.com>
2026-02-01 20:36:37 +0300

    Add FlightState enum for mission states

---
+++ MissionState.h
@@ -0,0 +1,12 @@
+enum class FlightState{
+    IDLE,
+    TAKEOFF,
+    MAPPING,
+    HOLD,              
+    LOW_BATTERY,
+    RETURN_TO_BASE,
+    SEARCHING_MARKER, 
+    PRECISION_LAND,
+    LANDED,
+    EMERGENCY_LAND,    
+    FAILSAFE};
